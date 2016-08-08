1a4036397c70a126b4485a561ce1028e58753f08
HsinWei <fatjuan2000@hotmail.com>
2016-08-08 11:01:28 +0800

    SQL DB save PATH

--- app/src/main/java/com/work/hsinwei/my0808_1_sqlite/MainActivity.java
+++ app/src/main/java/com/work/hsinwei/my0808_1_sqlite/MainActivity.java
@@ -2,6 +2,9 @@ package com.work.hsinwei.my0808_1_sqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        CopyDatabase();
     }
 
-    
+    private void CopyDatabase()
+    {
+        final int BUFFER_SIZE=400000;
+        final String DB_NAME = "test1.db";   //保存的資料庫檔案名
+        String PACKAGE_NAME = getPackageName();
+        File DB_PATH = getDatabasePath(DB_NAME);
+        Log.d("T0808-DB",DB_PATH.toString());
+    }
 }
