17f1a5d5e718ca33d9c362b14cf42d2f24c96bf5
HsinWei <fatjuan2000@hotmail.com>
2016-08-08 11:38:19 +0800

    SQL DB save PATH madir PATH

--- app/src/main/java/com/work/hsinwei/my0808_1_sqlite/MainActivity.java
+++ app/src/main/java/com/work/hsinwei/my0808_1_sqlite/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.util.Log;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,14 +18,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         CopyDatabase();
+
+
     }
 
     private void CopyDatabase()
     {
         final int BUFFER_SIZE=400000;
-        final String DB_NAME = "test1.db";   //保存的資料庫檔案名
+        final String DB_NAME = "test1.sqlite";   //保存的資料庫檔案名
         String PACKAGE_NAME = getPackageName();
         File DB_PATH = getDatabasePath(DB_NAME);
         Log.d("T0808-DB",DB_PATH.toString());
+
+        if(!(DB_PATH.exists()))
+        {
+            File DIR = new File(getApplicationInfo().dataDir+"/databases");
+            DIR.mkdir();
+            Log.d("T0808-DB","DIR:"+DIR.toString());
+
+            InputStream is = getResources().openRawResource(R.raw.test1);
+            try {
+                FileOutputStream fos = new FileOutputStream(DB_PATH);
+
+                byte[] buffer = new byte[BUFFER_SIZE];
+                int count = 0;
+                while ((count = is.read(buffer))>0)
+                {
+                    fos.write(buffer,0,count);
+                }
+                fos.close();
+                is.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
